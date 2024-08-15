c9cee68ab7eb9121c9698f3c6aaff7ce3a975885
Nicholas Russell <nicholas.russell@nottingham.ac.uk>
2024-08-15 11:53:52 +0100

    Fix how coeffs are calculated

--- prior-inputs.js
+++ prior-inputs.js
@@ -32,23 +32,24 @@ const truncLimits = {
 const formData = new FormData(document.getElementById("predictionForm"));
 const parous = formData.get("age") === "parous";
 
+const age = parseFloat(formData.get("age"));
 
 export const priorForm = {
   age: Math.max(
     truncLimits.age.lower,
-    Math.min(parseFloat(formData.get("age")), truncLimits.age.upper)
+    Math.min(age > 35 ? age - 35 : 0, truncLimits.age.upper)
   ),
   weight: Math.max(
     truncLimits.weight.lower,
-    Math.min(parseFloat(formData.get("weight")), truncLimits.weight.upper)
+    Math.min(parseFloat(formData.get("weight") - 69), truncLimits.weight.upper)
   ),
   height: Math.max(
     truncLimits.height.lower,
-    Math.min(parseFloat(formData.get("height")), truncLimits.height.upper)
+    Math.min(parseFloat(formData.get("height") - 164), truncLimits.height.upper)
   ),
   ethnicity: formData.get("ethnicity") === "white" ? 0 : 1,
   familyHistoryPE: formData.get("fam-hist-pe") === "yes" ? 1 : 0,
-  conception: formData.get("conception") === "spontaneous" ? 0 : 1,
+  conception: formData.get("conception") === "ivf" ? 1 : 0,
   chronicHypert: formData.get("chronic-hypert") === "yes" ? 1 : 0,
   diabetes: formData.get("diabetes") === "yes" ? 1 : 0,
   autoimmune: formData.get("autoimmune") === "yes" ? 1 : 0,
@@ -70,53 +71,53 @@ export const priorForm = {
     : 0,
 };
 
+// TODO correctly implement the parous coefficients
 export const priorCoeffs = {
   constant: 54.3637,
-  age: priorForm.age > 35 ? -0.206886 * priorForm.age : 0,
-  height: 0.11711 * (priorForm.height - 164),
+  age: -0.206886,
+  height: 0.11711,
 
-  ethnicityBlack: formData.get("ethnicity") === "black" ? -2.6786 : 0,
-  ethnicitySouthAsian: formData.get("ethnicity") === "south_asian" ? -1.129 : 0,
+  ethnicity: formData.get("ethnicity") === "black" ? -2.6786 : -1.129,
 
-  chronicHypert: priorForm.chronicHypert * -7.2897,
-  autoimmune: priorForm.autoimmune * -3.0519,
-  conceptionIVF: formData.get("conception") === "ivf" ? -1.6327 : 0,
+  chronicHypert: -7.2897,
+  autoimmune: -3.0519,
+  conception:  -1.6327,
 
-  parousWithPE: formData.get("parity") === "parous_with_pe" ? -8.1667 : 0,
+  parousWithPE: formData.get("parity") === "parous-with-pe" ? -8.1667 : 0,
   parousWithPEGestAge:
-  formData.get("parity") === "parous_with_pe"
+  formData.get("parity") === "parous-with-pe"
       ? 0.0271988 * Math.pow(priorForm.prevGestationAge - 24, 2)
       : 0,
-  parousNoPE: formData.get("parity") === "parous_no_pe" ? -4.335 : 0,
+  parousNoPE: formData.get("parity") === "parous-no-pe" ? -4.335 : 0,
   parousNoPEInterval:
-  formData.get("parity") === "parous_no_pe"
+  formData.get("parity") === "parous-no-pe"
       ? -4.1513765 * (priorForm.pregnancyInterval - 1)
       : 0,
   parousNoPEIntervalSquared:
-  formData.get("parity") === "parous_no_pe"
+  formData.get("parity") === "parous-no-pe"
       ? 9.21473572 * (priorForm.pregnancyInterval - 0.5)
       : 0,
   parousNoPEGestationalAge:
-  formData.get("parity") === "parous_no_pe"
+  formData.get("parity") === "parous-no-pe"
       ? 0.01549673 * Math.pow(priorForm.prevGestationAge - 24, 2)
       : 0,
 
   weight:
     priorForm.chronicHypert === 0
-      ? -0.0694096 * (priorForm.weight - 69)
+      ? -0.0694096
       : 0,
   familyHistoryPE:
     priorForm.chronicHypert === 0
-      ? -1.7154 * priorForm.familyHistoryPE
+      ? -1.7154
       : 0,
   diabetes:
-    priorForm.chronicHypert === 0 ? -3.3899 * priorForm.diabetes : 0,
+    priorForm.chronicHypert === 0 ? -3.3899 : 0,
 };
 
 function addParityEventListeners() {
   document.addEventListener("DOMContentLoaded", function () {
     var paritySelect = document.getElementById("parity");
-    var parousInputs = document.getElementById("parousInputs");
+    var parousInputs = document.getElementById("parous-inputs");
 
     function toggleParousInputs() {
       if (paritySelect.value === "nulliparous") {
